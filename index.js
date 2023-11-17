e86073c465ff397ddc98a5b3b3f7ab05a891dc3b
Ukkarawin Pansuparak <Ukkarawin.P@bitazza.com>
2023-11-17 07:31:01 +0700

    fix: add js file

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ApexWebSocket = void 0;
+var apex_ws_1 = require("./src/apex-ws");
+Object.defineProperty(exports, "ApexWebSocket", { enumerable: true, get: function () { return apex_ws_1.ApexWebSocket; } });
+exports.default = apex_ws_1.ApexWebSocket;
